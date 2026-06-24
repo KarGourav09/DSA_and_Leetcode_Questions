957a7bb5a0cda9392226dee754cf7b8ead3bdceb
GouravKar <kar541049@gmail.com>
2026-06-24 19:17:41 +0530

    new file:   step-05-strings/Basic and Easy Problems/Rotate_string.cpp

---
+++ step-05-strings/Basic and Easy Problems/Rotate_string.cpp
@@ -0,0 +1,61 @@
+/*796. Rotate String, Easy
+Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+ 
+
+Example 1:
+
+Input: s = "abcde", goal = "cdeab"
+Output: true
+Example 2:
+
+Input: s = "abcde", goal = "abced"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length, goal.length <= 100
+s and goal consist of lowercase English letters
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        for(int i = 0; i < s.length(); i++) {
+            if (s == goal) {
+                return true;
+            }
+            // Perform a left shift on s
+            char firstChar = s[0];
+            s.erase(0, 1); // Remove the first character
+            s.push_back(firstChar); // Append the first character to the end
+        }
+        return false;
+    }
+};
+
+int main() {
+    Solution solution;
+    string s = "abcde";
+    string goal = "cdeab";
+    bool result = solution.rotateString(s, goal);
+    cout << (result ? "true" : "false") << endl; // Output: true
+
+    s = "abcde";
+    goal = "abced";
+    result = solution.rotateString(s, goal);
+    cout << (result ? "true" : "false") << endl; // Output: false
+
+    return 0;
+}
